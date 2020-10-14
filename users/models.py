e3073d138822176d89f1682091ac07bb9b38997a
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-10-14 23:27:06 +0100

    updated the html and css for the user app

--- users/models.py
+++ users/models.py
@@ -9,6 +9,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     details = models.TextField(max_length=200, default='No profile details set yet by the user')
     image = models.ImageField(default='avatar.png', upload_to='profile_pics/')
+
+    @property
+    def photo_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        else:
+            return "/media/avatar.png"
     """
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
