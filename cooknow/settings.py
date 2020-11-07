c3b5907d0d34ef2056f31df1500a0b55d25154b8
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-11-07 06:13:46 +0100

    error persist

--- cooknow/settings.py
+++ cooknow/settings.py
@@ -181,8 +181,7 @@ AWS_S3_REGION_NAME='us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 AWS_DEFAULT_UCL=None
-if not (DEBUG):
-    DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 
 
