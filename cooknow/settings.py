4a2e5f73e39881c6d73c0afac5ee4db049153fc8
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-12-09 12:25:47 +0100

    fixed and run migrate

--- cooknow/settings.py
+++ cooknow/settings.py
@@ -30,8 +30,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
+# DEBUG = True
 
 ALLOWED_HOSTS = ["cookella.herokuapp.com", 'localhost', '127.0.0.1']
 
