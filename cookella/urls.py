eaa4aa7c2dd4112477ce21d45e370f6319ab5d8e
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-01-11 09:21:48 +0100

    updated url to point to search view

--- cookella/urls.py
+++ cookella/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path(r'', views.index, name='cookella-index'),
     path(r'about/', views.about, name='cookella-about'),
     path('dishes/', FoodListView.as_view(), name="dish_list"),
+    path ('recipes/search/', views.recipe_search, name="recipe_search"),
     path('dishes/<int:pk>/<slug>/', FoodDetailView.as_view(), name="recipes"),
     path(r'dishes/add-recipe/', views.contributor, name='cookella-add-recipe'),
     path(r'dishes/<int:pk>/<slug:slug>/update/', views.recipeUpdateView, name='recipe-update'),
