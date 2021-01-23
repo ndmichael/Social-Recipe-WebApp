f39d72fa40678879ba23087a28aa542ce05956a2
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-01-23 06:42:52 +0100

    added form override for signup form, removing placeholders and add large input

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,26 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from allauth.account.forms import SignupForm
+
+
+class SelfSignUpForm (SignupForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["email"].label = ""
+        self.fields["email"].widget.attrs.update({'class': 'form-control-lg'})
+
+        self.fields["username"].label = ""
+        self.fields["username"].widget.attrs.update({'class': 'form-control-lg'})
+
+        self.fields["password1"].label = ''
+        self.fields["password1"].widget.attrs.update({'class': 'form-control-lg'})
+        
+        self.fields["password2"].label = '' 
+        self.fields["password2"].widget.attrs.update({'class': 'form-control-lg'})
+
+        # self.fields["email"].label = ""
 
 
 class UserRegisterForm(UserCreationForm):
