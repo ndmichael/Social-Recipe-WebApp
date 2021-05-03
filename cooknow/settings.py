097b97a0eeff8a1004fdafa9619a11fcfb2e1066
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-05-03 19:18:23 +0100

    updated settings

--- cooknow/settings.py
+++ cooknow/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 't567@n+)9x=1x1&1rs5v6j6@y)6nj#onpabm!d^06oyb%vofhr'
+SECRET_KEY = 't567@n+)9x=1x1&1rs5v6j6@y)6nj#onpabm!d^06oyb%vofhr'
 
-SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA') 
+# SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
