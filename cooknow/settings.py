e798de2b1c82117bb268ce632894647e08d23d0d
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-07-07 11:44:36 +0100

    add nokrecipes to alowed hosts

--- cooknow/settings.py
+++ cooknow/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 
-print((os.environ.get('DEBUG_VALUE') == 'True'))
-
+print((os.environ.get("DEBUG_VALUE") == "True"))
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,15 +24,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't567@n+)9x=1x1&1rs5v6j6@y)6nj#onpabm!d^06oyb%vofhr'
+SECRET_KEY = "t567@n+)9x=1x1&1rs5v6j6@y)6nj#onpabm!d^06oyb%vofhr"
 
-# SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA') 
+# SECRET_KEY=os.environ.get('SECRET_KEY_COOKELLA')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
+DEBUG = os.environ.get("DEBUG_VALUE") == "True"
 # DEBUG = True
 
-ALLOWED_HOSTS = ["cookella.herokuapp.com", 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "cookella.herokuapp.com",
+    "http://nokrecipes.com/",
+    "localhost",
+    "127.0.0.1",
+]
 
 SITE_ID = 2
 
@@ -41,73 +45,71 @@ SITE_ID = 2
 # Application definition
 
 INSTALLED_APPS = [
-    'cookella.apps.CookellaConfig',
-    'users.apps.UsersConfig',
-    'blog.apps.BlogConfig',
-    'crispy_forms',
-    'django_countries',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "cookella.apps.CookellaConfig",
+    "users.apps.UsersConfig",
+    "blog.apps.BlogConfig",
+    "crispy_forms",
+    "django_countries",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # allauth
-    'allauth', 
-    'allauth.account', 
-    'allauth.socialaccount', 
-    # 'allauth.socialaccount.providers.google', 
-    # 'allauth.socialaccount.providers.facebook', 
-    'django.contrib.sites',
-
-     
-    'storages',
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    # 'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.facebook',
+    "django.contrib.sites",
+    "storages",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'cooknow.urls'
+ROOT_URLCONF = "cooknow.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'cooknow.wsgi.application'
+WSGI_APPLICATION = "cooknow.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    "default": {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('COOKELLA_DB_NAME') ,
-        'USER': os.environ.get('COOKELLA_DB_USER') ,
-        'PASSWORD': os.environ.get('COOKELLA_DB_PW') ,
-        'HOST': 'localhost',
-        'PORT': '5432',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("COOKELLA_DB_NAME"),
+        "USER": os.environ.get("COOKELLA_DB_USER"),
+        "PASSWORD": os.environ.get("COOKELLA_DB_PW"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -117,16 +119,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -134,9 +136,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -149,65 +151,62 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = []
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-LOGIN_REDIRECT_URL = 'blog-home'
-LOGIN_URL = 'account_login'
+LOGIN_REDIRECT_URL = "blog-home"
+LOGIN_URL = "account_login"
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 # setting environmental variables for S3
 
-AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID_COOKELLA') 
-AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY_COOKELLA') 
-AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME_COOKELLA') 
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID_COOKELLA")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY_COOKELLA")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME_COOKELLA")
 
 
-AWS_S3_FILE_OVERWRITE=False
-AWS_S3_REGION_NAME='us-east-2'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_REGION_NAME = "us-east-2"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
 
-AWS_DEFAULT_UCL=None
+AWS_DEFAULT_UCL = None
 
 if not DEBUG:
-    DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
-
-#django-allauth registraion settings 
+# django-allauth registraion settings
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 
 
-ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS =1
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "optional"
 ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = "blog-home"
 
-# 1 day 
+# 1 day
 ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400
 
-#or any other page 
-ACCOUNT_LOGOUT_REDIRECT_URL ='/accounts/login/'
+# or any other page
+ACCOUNT_LOGOUT_REDIRECT_URL = "/accounts/login/"
 
-ACCOUNT_FORMS = {'signup': 'users.forms.SelfSignUpForm'}
+ACCOUNT_FORMS = {"signup": "users.forms.SelfSignUpForm"}
 
 django_heroku.settings(locals())
 
