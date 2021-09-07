bafdcb917acf36dbf6c217500602dd3444db8734
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-09-07 20:57:25 +0100

    created view for profile settings

--- users/views.py
+++ users/views.py
@@ -6,20 +6,23 @@ from django.contrib.auth.models import User
 from blog.models import Post
 from cookella.models import FoodType
 from django.views.generic import ListView
+
 # Create your views here.
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account has been created for {username} you  can now login.')
-            return redirect('login')
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Account has been created for {username} you  can now login."
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, "users/register.html", {"form": form})
 
 
 # the profile view now receive an attribute which is the current username
@@ -27,52 +30,64 @@ def register(request):
 # i could use the get absolute url on the model but it doesn't update the profile
 @login_required
 def profile(request, username):
-    user = get_object_or_404(User, username=username)  # getting the current user passed to it
+    user = get_object_or_404(
+        User, username=username
+    )  # getting the current user passed to it
     user_post = Post.objects.filter(author=user)
     contribution_count = FoodType.objects.filter(contributor=user).count()
     post_count = user_post.count()
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES,  instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'account successfully updated')
-            return redirect('profile', username=request.user.username)  # i solved that by passing the requesting user username
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
-        'u_form': u_form,
-        'p_form': p_form,
-        'user': user,
-        'post_count': post_count,
-        'contribution_count': contribution_count,
-
-
+        "user": user,
+        "post_count": post_count,
+        "contribution_count": contribution_count,
     }
-    return render(request, 'users/profile.html', context)
+    return render(request, "users/profile.html", context)
 
 
 # handles the view for the user list template.
 class UserPostListView(ListView):
     model = Post
-    template_name = 'blog/user_post.html'
-    context_object_name = 'posts'
+    template_name = "blog/user_post.html"
+    context_object_name = "posts"
     paginate_by = 5
 
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))  # getting the username on the fly
-        return Post.objects.filter(author=user).order_by('-date_created')
+        user = get_object_or_404(
+            User, username=self.kwargs.get("username")
+        )  # getting the username on the fly
+        return Post.objects.filter(author=user).order_by("-date_created")
 
 
 # handles the view for the user contribution list template.
 class ContributorListView(ListView):
     model = FoodType
-    template_name = 'users/user_contribution.html'
+    template_name = "users/user_contribution.html"
     paginate_by = 4
 
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))  # getting the username on the fly
-        return FoodType.objects.filter(contributor=user).order_by('-date_added')
+        user = get_object_or_404(
+            User, username=self.kwargs.get("username")
+        )  # getting the username on the fly
+        return FoodType.objects.filter(contributor=user).order_by("-date_added")
+
+
+def profile_settings(request, username):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"account successfully updated")
+            return redirect(
+                "profile", username=request.user.username
+            )  # i solved that by passing the requesting user username
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "users/profile_settings.html", context)
