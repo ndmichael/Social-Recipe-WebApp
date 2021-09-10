0187739a0dfa9136eac4a37c05314b894510a8a6
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-09-10 17:03:34 +0100

    updated main url

--- cookella/urls.py
+++ cookella/urls.py
@@ -4,14 +4,16 @@ from .views import FoodListView, FoodDetailView
 from . import views
 
 
-
 urlpatterns = [
-    path(r'', views.index, name='cookella-index'),
-    path(r'about/', views.about, name='cookella-about'),
-    path('dishes/', FoodListView.as_view(), name="dish_list"),
-    path ('recipes/search/', views.recipe_search, name="recipe_search"),
-    path('dishes/<int:pk>/<slug>/', FoodDetailView.as_view(), name="recipes"),
-    path(r'dishes/add-recipe/', views.contributor, name='cookella-add-recipe'),
-    path(r'dishes/<int:pk>/<slug:slug>/update/', views.recipeUpdateView, name='recipe-update'),
+    path(r"", views.index, name="cookella-index"),
+    path(r"about/", views.about, name="cookella-about"),
+    path("dishes/", FoodListView.as_view(), name="dish_list"),
+    path("recipes/search/", views.recipe_search, name="recipe_search"),
+    path("dishes/<int:pk>/<slug>/", FoodDetailView.as_view(), name="recipes"),
+    path(r"dishes/add-recipe/", views.contributor, name="cookella-add-recipe"),
+    path(
+        r"dishes/<int:pk>/<slug:slug>/update/",
+        views.recipeUpdateView,
+        name="recipe-update",
+    ),
 ]
-
