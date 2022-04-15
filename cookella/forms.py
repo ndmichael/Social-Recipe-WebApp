1d1abc0259d97b5a56fd34bea1a35e7251616f3d
michael codesmith <up811786@student.reading.ac.uk>
2022-04-15 09:31:07 +0100

    updated model, allowing users to add category for recipe

--- cookella/forms.py
+++ cookella/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import FoodRecipe, FoodType
+from .models import FoodRecipe, FoodType, Category
+
+categories = Category.objects.all().values_list("name", "name")
+categories_list = [category for category in categories]
 
 
 class FoodTypeForm(forms.ModelForm):
+    category = forms.ChoiceField(initial="soup", choices=categories_list)
+
     class Meta:
         model = FoodType
-        fields = ["food_name", "country"]
+        fields = ["category", "food_name", "country"]
+
+        widgets = {}
 
 
 class FoodRecipeForm(forms.ModelForm):
